040adcebb93caae57d2323e3d981007c458969cf
Yugal Modi <yugal.modi@gmail.com>
2022-01-31 16:43:42 -0600

    W3D4 - minor bug fix

--- src/node/w3d4_excercise/scripts/AdderWebServer.js
+++ src/node/w3d4_excercise/scripts/AdderWebServer.js
@@ -4,9 +4,11 @@ var fs = require('fs');
 var addmod = require('./addmod.js');
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    if (q.pathname=="/add.js") addmod.add(req,res,q.query)
-    else fs.readFile(filename, function(err, data) {
+    var filename = q.pathname;
+    console.log(filename);
+    if (q.pathname=="./add.js") addmod.add(req,res,q.query)
+    else fs.readFile('./w3d4_excercise/'+filename, function(err, data) {
+        console.log(err);
     if (err) {
         res.writeHead(404, {'Content-Type': 'text/html'});
         return res.end("404 Not Found");
